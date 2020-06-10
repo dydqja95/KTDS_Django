083ebc24cbace38b4510a33a91c2e7bbc91e5267
dydqja95 <49568167+dydqja95@users.noreply.github.com>
2020-06-10 17:06:30 +0900

    Update models.py

--- Jango/bookmark/models.py
+++ Jango/bookmark/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Bookmark(models.Model):
     site_name = models.CharField(max_length = 100)# char인 100byte
@@ -7,5 +8,6 @@ class Bookmark(models.Model):
     def __str__(self):
         return "이름 : " + self.site_name + ", 주소 : " + self.url
 
-    # def get_absolute_url(self):
-    #     return reverse('detail', args=[self.id])
+    def get_absolute_url(self):
+        # reverse 메소드는 URL 패턴의 이름과 추가 인자를 전달받아 URL생성
+        return reverse('detail', args=[self.id])
